bf67da6de992aeb1365fa5d2c58713a131116262
Anthony Ng <newyork.anthonyng@gmail.com>
2016-12-20 21:52:21 -0800

    Close portal when Transport is closed

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # react-transport
 
 A component that transports HTML elements to document.body.
+
+```javascript
+<Transport isOpen>
+	<h1>Hello World</h1>
+</Transport>
+
+// The H1 element will be appended to the end of document.body
+```
+
+# API
+isOpen : A React Component property that takes a boolean. If true, the element will be removed from the normal HTML flow and appended to the end of the document.body. If false, then nothing will render.
+
+children : A single React element.
--- hello.js
+++ hello.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Transport from './lib/Transport';
+
+const Goodbye = ({ onClick }) => (
+	<h1 onClick={onClick}>Goodbye</h1>
+);
 
 class Hello extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isVisible: false,
+			counter: 0
+		};
+
+		this.handleButtonClick = this.handleButtonClick.bind(this);
+		this.test = this.test.bind(this);
+	}
+
+	handleButtonClick() {
+		this.setState({
+			isVisible: !this.state.isVisible,
+			counter: this.state.counter + 1
+		});
+	}
+
+	test() {
+		alert(`Hello World: ${this.state.counter}`);
+	}
+
 	render() {
-		return <h1>Hello world</h1>
+		return (
+			<div>
+				<button onClick={this.handleButtonClick}>Click Me</button>
+				<Transport isOpen={this.state.isVisible}>
+					<Goodbye onClick={this.test} />
+				</Transport>
+				<h1>Hello World</h1>
+			</div>
+		);
 	}
 }
 
